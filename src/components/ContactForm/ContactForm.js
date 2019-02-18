cd9340ef6bb339a93b103746eadf98b7283d8aea
oliviergenevest <olivier.genevest@gmail.com>
2019-02-18 21:37:13 +0100

    netlify forms

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -8,16 +8,16 @@ const ContactForm = () =>
 		    <input type="hidden" name="bot-field" />
 		    <div className="field half first">
 		        <label htmlFor="name">Name</label>
-		        <input placeholder="Your Name" name="name" id="name" />
+		        <input placeholder="Your Name" name="name" id="name" required />
 		      
 		    </div>
 		    <div className="field half">
 		        <label htmlFor="email">Email</label>
-		        <input placeholder="Your Email Address"  type="text" name="email" id="email" />
+		        <input placeholder="Your Email Address"  type="text" name="email" id="email" required  />
 		    </div>
 		    <div className="field">
 		        <label htmlFor="message">Message</label>
-		        <textarea name="message" id="message" rows="6"></textarea>
+		        <textarea name="message" id="message" rows="6" required ></textarea>
 		    </div>
 		    <ul className="actions">
 		        <li><input type="submit" value="Send Message" className="special" /></li>
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import Container from '../components/Container/Container'
 
 
 const Success = (props) => (
@@ -10,17 +11,13 @@ const Success = (props) => (
             <meta name="description" content="Success Page" />
         </Helmet>
 
-        <div id="main" className="alt">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h1>Success/Thank You Page</h1>
-                    </header>
-                  
-                    <p>Thank you for contacting me!</p>
-                </div>
-            </section>
-        </div>
+        <Container text>
+            
+            <h1>Success</h1>
+            <p>Thank you for contacting me!</p>
+       
+          
+        </Container>
     </Layout>
 )
 
