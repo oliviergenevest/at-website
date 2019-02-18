0a4d368ee02dcc3514a6aa7b16342558c38720d8
oliviergenevest <olivier.genevest@gmail.com>
2019-02-18 20:19:29 +0100

    form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,7 +3,7 @@ const ContactForm = () =>
 (
 	<>
 		<h2>Contact</h2>
-		<form method="post" action="#"data-netlify="true" data-netlify-honeypot="bot-field">
+		<form name="contact" method="post"  data-netlify="true" data-netlify-honeypot="bot-field">
 		    <input type="hidden" name="bot-field" />
 		    <div className="field half first">
 		        <label htmlFor="name">Name</label>
