f87d39103ed0c6900e8a35eb57711c3df08d35e9
oliviergenevest <olivier.genevest@gmail.com>
2019-04-02 13:13:02 +0200

    change facebook address

--- src/config/SiteConfig.js
+++ src/config/SiteConfig.js
@@ -13,7 +13,7 @@ module.exports = {
    publisher:'Alan Tod',
     shareImage: '/images/alan-tod-icon.png', // Open Graph Default Share Image. 1200x1200 is recommended
   //siteFBAppID: '123456789', // Facebook App ID
-  facebookPageUrl:'https://www.facebook.com/julien.isore.7',
+  facebookPageUrl:'https://www.facebook.com/Alan-Tod-660829114058650/',
   instagramPageUrl:'https://www.instagram.com/alan.tod',
   //userTwitter: '@emma', // Twitter Username
   // og: Meta Tags
